ca8bd25e9d252540140458f8ed15dc142223d389
isavon <isavon.we@gmail.com>
2022-07-11 07:38:20 +0300

    added jquery-ui

--- src/SortableGridAsset.php
+++ src/SortableGridAsset.php
@@ -13,11 +13,16 @@ class SortableGridAsset extends AssetBundle
 {
     public $sourcePath = '@isavon/sortablegrid/assets';
 
+    public $css = [
+        'vendor/jquery-ui/css/jquery-ui.css',
+    ];
+
     public $js = [
+        'vendor/jquery-ui/js/jquery-ui.min.js',
         'js/jquery.sortable.gridview.js',
     ];
 
     public $depends = [
-        'backend\assets\JuiAsset',
+        'yii\web\YiiAsset',
     ];
 }
